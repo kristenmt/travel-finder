7fbf58e0935e980920f780001db4d35129aa333c
Kristen Truyol <kristentruyol@Kristens-Air.centurylink.net>
2018-08-21 18:05:43 -0400

    added countries and their data to the js file

---
+++ app/data/countries.js
@@ -0,0 +1,28 @@
+// data for countries
+
+var countries = [
+    {
+        name: "Tahiti",
+        description: "Money is no object, and you prefer relaxing in the sun."
+    },
+    {
+        name: "France",
+        description: "You love all things food. Art, culture, history, and sightseeing get your blood pumping. You don't mind going out of your comfort zone. Fitness? Walking all over Paris or the French countryside is all you need to worry about."
+    },
+    {
+        name: "Switzerland",
+        description: "You love snow-related activities. Visiting other countries is a great way to explore your world, and try new foods. You don't mind going out of your comfort zone. You enjoy staying fit while on vacation."
+    },
+    {
+        name: "Thailand",
+        description: "You love going out of your comfort zone. New foods and new cultures fascinate and excite you. You have places to visit--you'll relax when you're dead."
+    },
+    {
+        name: "Kenya",
+        description: "Totally new cultures, food, and adventures rule your vacations. Comfort zone? What's that? You love trying new things. Heat and sun don't bother you at all."
+    },
+    {
+        name: "Florida, USA",
+        description: "You prefer to explore your own country. You are on a budget when you travel, so you need to stick close to home. You love sun and surf, and much prefer to relax and take it easy by the beach. Culture? You consider the beach and a drink to be all the culture you need!"
+    }
+]
