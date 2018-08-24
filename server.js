b1d17a580352eda6d3d224937246131bd1d56b87
Kristen Truyol <kristentruyol@Kristens-Air.centurylink.net>
2018-08-24 17:07:45 -0400

    another test to get image working

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 //require routes in folder
 require('./app/routing/apiRoutes')(app)
 require('./app/routing/htmlRoutes')(app)
